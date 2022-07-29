bc933eb358fdc8dd31805562b9ed1d030588b198
Amr Mahmoud <amrshedou@gmail.com>
2022-07-29 18:18:53 +0000

    finish database optimisation

--- blango/settings.py
+++ blango/settings.py
@@ -64,13 +64,17 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         "crispy_forms",
         "crispy_bootstrap5",
+        "debug_toolbar",
         'blog',
     ]
 
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
     CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+    INTERNAL_IPS = ["192.168.11.179"]
+
     MIDDLEWARE = [
+         "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
